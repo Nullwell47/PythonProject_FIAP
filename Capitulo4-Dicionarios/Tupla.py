9afb7398ab35e1ac2b8ad399b05face50fe9e5d4
Emanoel Rosa Barros <55563138+emanoel47@users.noreply.github.com>
2022-03-30 05:26:45 -0300

    Add files via upload

---
+++ Capitulo4-Dicionarios/Tupla.py
@@ -0,0 +1,12 @@
+ips={}
+resp="S"
+while resp =="S":
+    ips[(input("Digite os dois primeiros octetos: "),
+         input("Digite os dois últimos octetos: "))]=input("Nome da máquina: ")
+    resp=input("Digete <S> para continuar: ").upper()
+
+print("Exibindo as máquinas que compõem uma mesma rede: ")
+rede=input("Digite os dois primeiros octetos: ")
+for ip,nome in ips.items():
+    if(ip[0]==rede):
+        print(nome)
